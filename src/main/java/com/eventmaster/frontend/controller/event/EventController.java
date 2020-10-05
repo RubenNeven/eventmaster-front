ae07a5993710630f428b497e23fb07a5c2be73ce
RubenNeven <rubenneven@gmail.com>
2020-10-05 16:35:29 +0200

    Eventform + Postmapping frontend

--- src/main/java/com/eventmaster/frontend/controller/event/EventController.java
+++ src/main/java/com/eventmaster/frontend/controller/event/EventController.java
@@ -9,10 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -42,7 +45,10 @@ public class EventController {
     }
 
     @PostMapping("/new")
-    public String addEventForm(@ModelAttribute("event") Event event){
+    public String addEventForm(@Valid @ModelAttribute("event") Event event, BindingResult br){
+        if (br.hasErrors()){
+            return "addEvent";
+        }
         eventService.addEvent(eventMapper.map(event));
         return "redirect:/events/new";
     }
--- src/main/java/com/eventmaster/frontend/model/Event.java
+++ src/main/java/com/eventmaster/frontend/model/Event.java
@@ -1,11 +1,11 @@
 package com.eventmaster.frontend.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
@@ -15,8 +15,15 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class Event {
     private Long id;
+
+    @NotBlank(message = "Event name is required")
+    @NotNull(message = "Event name is required")
+    @Size(min = 1)
     private String name;
     private BigDecimal price;
+
+    @NotBlank(message = "Location is required")
+    @Size(min = 1, message = "Location is required")
     private String location;
     @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
     private LocalDateTime dateFrom;
